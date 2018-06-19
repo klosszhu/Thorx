6331889a2e07a4fe2b38fd1b52fd57a9b4d94f25
klosszhu <314963663@qq.com>
2018-06-19 09:00:23 +0800

    11

--- MongoTest/Form1.cs
+++ MongoTest/Form1.cs
@@ -6,13 +6,19 @@ using System.Data;
 using System.Diagnostics;
 using System.Drawing;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using Thorx.MongoResponsitory;
 using Thorx.MongoToolkit;
+
+
+
+[assembly: SuppressIldasmAttribute()]
 namespace MongoTest
 {
+   
     public partial class Form1 : Form
     {
         public Form1()
--- MongoTest/Program.cs
+++ MongoTest/Program.cs
@@ -3,9 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Runtime.CompilerServices;
+
+
 
 namespace MongoTest
 {
+
     static class Program
     {
         /// <summary>
